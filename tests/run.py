59eca5c9be74b553108f5e5435c84a4477dc6e08
Arturo Yepez <arturyepez10@gmail.com>
2022-06-11 19:55:49 -0400

    primeros casos de prueba

---
+++ tests/run.py
@@ -0,0 +1,9 @@
+# ------------------------ IMPORTS ----------------------------- #
+# Libraries
+import unittest
+
+# UNIT TEST SUITES
+
+# ------------------------ RUN ----------------------------- #
+if __name__ == '__main__':
+  unittest.main()
